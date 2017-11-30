130a627c765ad4033449d8b08217e1edd8940aea
FunGamesYT <redmineboys@gmail.com>
2017-11-30 14:58:32 +0100

    Added intent to open Csv files in an other application

--- app/src/main/java/com/fungames/privateservercreationtool/CardStatsFragment.java
+++ app/src/main/java/com/fungames/privateservercreationtool/CardStatsFragment.java
@@ -4,18 +4,22 @@ package com.fungames.privateservercreationtool;
  * Created by Fabian on 24.11.2017.
  */
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class CardStatsFragment extends Fragment {
@@ -44,12 +48,28 @@ public class CardStatsFragment extends Fragment {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Adapter adapter = adapterView.getAdapter();
                 CardStatsItem item = (CardStatsItem) adapter.getItem(i);
+                File csv = new File(item.getFilePath());
+                Intent intent = new Intent();
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(csv), getMimeType(csv.getAbsolutePath()));
+                startActivityForResult(intent, 10);
                 Toast.makeText(getContext(), item.getFilePath(), Toast.LENGTH_SHORT).show();
             }
         });
         ArrayAdapter<CardStatsItem> adapter = new ArrayAdapter<CardStatsItem>(csvList.getContext(),
                 android.R.layout.simple_list_item_1, android.R.id.text1, cardStatsItems.toArray(new CardStatsItem[0]));
         csvList.setAdapter(adapter);
+    }
+    private String getMimeType(String url)
+    {
+        String parts[]=url.split("\\.");
+        String extension=parts[parts.length-1];
+        String type = null;
+        if (extension != null) {
+            MimeTypeMap mime = MimeTypeMap.getSingleton();
+            type = mime.getMimeTypeFromExtension(extension);
+        }
+        return type;
     }
 
 }
